c8340789cc037d67fe44575efa768bfba4a3834f
MiguelAngel2022Sistemas <113484673+MiguelAngel2022Sistemas@users.noreply.github.com>
2023-07-15 01:05:37 +0000

    Guardar

---
+++ src/test/java/com/example20/example20/Example20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example20.example20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
